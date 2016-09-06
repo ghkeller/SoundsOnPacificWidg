da177505bda7bc213e706006edd44f127f602af9
ghkeller <ghkeller@ucsc.edu>
2016-09-06 11:13:56 -0700

    Made coordinate into variable

--- source/PairWidg.C
+++ source/PairWidg.C
@@ -15,8 +15,10 @@ PairWidg::PairWidg(WContainerWidget *parent)
 	Wt::WHBoxLayout *hbox2 = new Wt::WHBoxLayout();
 	container->setLayout(hbox2);
 
+	Wt::WGoogleMap::Coordinate c = Wt::WGoogleMap::Coordinate(36.973120,-122.025868);
+
 	Wt::WGoogleMap *map_ = new Wt::WGoogleMap(Wt::WGoogleMap::Version3);
-	map_->setCenter(Wt::WGoogleMap::Coordinate(36.973120,-122.025868));
+	map_->setCenter(c);
 	map_->addIconMarker(Wt::WGoogleMap::Coordinate(36.973120,-122.025868), "icons/sopwidg_sel.png");
 	map_->addIconMarker(Wt::WGoogleMap::Coordinate(36.974815,-122.026404), "icons/sopwidg_unsel.png");
 	for(int x = 0; x < 2; x++)
